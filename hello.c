8156676db091865c78679d0fa18e0f490d25c0e8
Miguel Reinoso <miguelreinoso7@gmail.com>
2019-08-30 12:50:17 +0200

    Hello World

---
+++ hello.c
@@ -0,0 +1,18 @@
+// =================================================
+// To compile and name the program the commands are:
+//   gcc program.c -o program  
+
+// To run the program:
+//   ./program
+// =================================================
+
+
+// Hello world
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello, world!\nThis is C!\n");
+	return 0;
+}
+
