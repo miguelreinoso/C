dd65a878ed1ded4d97ddac56a5589dbcea7f9d3c
Miguel Reinoso <miguelreinoso7@gmail.com>
2019-09-03 18:19:45 +0200

    Character input

---
+++ getchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char a = getchar();  /* input is stored in the variable 'a' */
+
+  printf("You entered: %c\n", a);  /* '\n' jumps to next line */
+
+  return 0;
+}
+
